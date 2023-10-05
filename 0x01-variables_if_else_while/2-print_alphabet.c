2cc0a744b6bc13ae290f891c774cfc631a762b62
Antonious-Awad <antonuostony1@gmail.com>
2023-10-06 00:09:38 +0300

    conditions and loops

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0
+ */
+
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
