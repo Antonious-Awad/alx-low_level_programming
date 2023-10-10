448eb5f18dc287f5e99e4394f092044148fbee3a
Antonious-Awad <antonuostony1@gmail.com>
2023-10-10 21:18:40 +0300

    Functions and loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+/**
+ * print_sign - prints sign
+ * @n: input number
+ * Return: 1, -1 or 0 based on in the input
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
