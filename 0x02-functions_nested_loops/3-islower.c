e8dae981afcd7ea688e8dfe76a7c506c3ee1ab60
Antonious-Awad <antonuostony1@gmail.com>
2023-10-10 21:08:37 +0300

    loop

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 #include <ctype.h>
 /**
  * _islower - checks if letter is lowercase
- *
+ * @c: character input
  * Return: 0 if not lowercase, 1 if lowercase
  */
 int _islower(int c)
 {
 	char character = c;
+
 	if (character <= 'z' && character >= 'a')
 		return (1);
 	else
