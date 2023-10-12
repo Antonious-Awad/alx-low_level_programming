6e61a7a25ca0ecff98eb3b67efe660500608fd81
Antonious-Awad <antonuostony1@gmail.com>
2023-10-12 18:39:29 +0300

    more functions and loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,9 +7,9 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 0; j <= size - i; j++)
 		{
 			_putchar(' ');
 		}
