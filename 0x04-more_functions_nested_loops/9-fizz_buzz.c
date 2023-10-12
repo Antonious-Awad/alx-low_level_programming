393adcffee0548a220ebb4de0a6f66b06d8eff24
Antonious-Awad <antonuostony1@gmail.com>
2023-10-12 18:19:57 +0300

    more functions and loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (!(i % 3))
+		if (!(i % 3) && !(i % 5))
+		{
+			printf("FizzBuzz");
+		}
+		else if (!(i % 3))
 		{
 			printf("Fizz");
 		}
 		else if (!(i % 5))
 		{
 			printf("Buzz");
-		}
-		else if (!(i % 3) && !(i % 5))
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
