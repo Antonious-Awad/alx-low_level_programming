8526ffaaebc3c1d0bfddc52826e7cb912e9f2830
Antonious-Awad <antonuostony1@gmail.com>
2023-10-16 22:37:03 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints reversed string
+ * @s: string pointer
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int n = strlen(s) - 1;
+	char sRev[n];
+	for (i = n; i >= 0; i--)
+	{
+		sRev[i] = s + i;
+	}
+	puts(sRev);
+}
