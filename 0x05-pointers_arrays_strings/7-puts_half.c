a511fe8eeac9220cc3bcad7a3b7c8805fd050045
Antonious-Awad <antonuostony1@gmail.com>
2023-10-16 23:22:31 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts_half - prints second half of string
+ * @str: string pointer
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	int n = strlen(str), half = n % 2 ? (n - 1) / 2 : n / 2;
+
+	for (i = half; i < n; i++)
+	{
+		_putchar(str[i]);
+	}
+}
