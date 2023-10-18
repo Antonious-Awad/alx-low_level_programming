8f625ef5572a28bb13f749f3ca87e0c53e0f345d
Antonious-Awad <antonuostony1@gmail.com>
2023-10-18 23:44:09 +0300

    pointers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcmp - compares a string
+ * @s1: string one
+ * @s2: string two
+ * Return: difference in comparison
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
