50449fb19e3da98218717fe1c0c2cda16aa03d9b
Antonious-Awad <antonuostony1@gmail.com>
2023-10-18 22:22:01 +0300

    pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
