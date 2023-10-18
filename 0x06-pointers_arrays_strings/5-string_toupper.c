038858169af831b2774fc030949d965564d3dbd4
Antonious-Awad <antonuostony1@gmail.com>
2023-10-19 00:01:04 +0300

    pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * string_toupper - returns an uppercased string
+ * @s: string
+ * Return: uppercase String
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s[i] = toupper(s[i]);
+	}
+}
