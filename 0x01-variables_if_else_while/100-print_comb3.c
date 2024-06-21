a2b93138b0a22aba7b78f8563988e5583e60d1d7
Antonious-Awad <antonuostony1@gmail.com>
2024-06-21 18:26:30 +0300

    chore: 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x < 58; x++)
+	{
+		for (y = 49; y < 58; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+
+				if (y <= 57 && x != 56)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	return (0);
+}
