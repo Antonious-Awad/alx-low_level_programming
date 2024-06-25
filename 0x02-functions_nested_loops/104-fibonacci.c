4fca41ff0352a6630ec675db50cf448c0e650170
Antonious-Awad <antonuostony1@gmail.com>
2024-06-25 15:21:06 +0300

    feat: 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,21 +7,28 @@
 
 int main(void)
 {
-	unsigned long long f1 = 1, f2 = 2, i;
-	unsigned long long next = f1 + f2;
+	unsigned long int m = 1, n = 2, i;
+	unsigned long int m1, m2, n1, n2;
 
-	printf("%lu, %lu, ", f1, f2);
-	for (i = 3; i <= 98; i++)
+	printf("%lu", m);
+	for (i = 1; i < 91; i++)
 	{
-		if (i == 50)
-			printf("%lu\n", next);
-		else
-		{
-			printf("%lu, ", next);
-			f1 = f2;
-			f2 = next;
-			next = f1 + f2;
-		}
+		printf(" ,%lu", n);
+		n += m;
+		m = n - m;
+	}
+	n1 = n / 1000000000;
+	n2 = n % 1000000000;
+	m1 = m / 1000000000;
+	m2 = m % 1000000000;
+	for (i = 92; i < 99; i++)
+	{
+		printf(", %lu", n1 + (n2 / 1000000000));
+		printf("%lu", n2 % 1000000000);
+		n1 += m1;
+		m1 = n1 - m1;
+		n2 += m2;
+		m2 = n2 - m2;
 	}
 	return (0);
 }
