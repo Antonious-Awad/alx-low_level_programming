67676ae5bc1c387055abd6dcdc0e9a1e60e57491
Antonious-Awad <antonuostony1@gmail.com>
2024-06-25 13:27:04 +0300

    chore: code fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,13 +13,13 @@ void print_times_table(int n)
 		return;
 	}
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			int prod = i * j;
 
-			if (j == 0 || i == 0)
+			if (j == 0)
 				_putchar('0');
 			else if (prod < 10)
 			{
