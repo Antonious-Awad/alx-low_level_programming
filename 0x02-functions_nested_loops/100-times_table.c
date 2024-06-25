0774a8d5246b01fb61c0f93777d0a443aef72eea
Antonious-Awad <antonuostony1@gmail.com>
2024-06-25 13:33:14 +0300

    chore: lint

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
 
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, prod;
 
 	if (n < 0 || n > 15)
 	{
@@ -17,30 +17,28 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			int prod = i * j;
-
+			prod = i * j;
 			if (j == 0)
 				_putchar('0');
-			else if (prod < 10)
+			if (j != n)
 			{
 				_putchar(',');
 				_putchar(' ');
+			}
+			if (prod < 10)
+			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(prod + '0');
 			}
 			else if (prod < 100)
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 				_putchar(prod / 10 + '0');
 				_putchar(prod % 10 + '0');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(prod / 100 + '0');
 				_putchar((prod % 100) / 10 + '0');
 				_putchar(prod % 10 + '0');
