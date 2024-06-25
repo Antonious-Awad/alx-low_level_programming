7f244f81fa84b3a224eaf7aaef637cffed59688c
Antonious-Awad <antonuostony1@gmail.com>
2024-06-25 14:47:23 +0300

    feat: 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	unsigned long int f1 = 1, f2 = 2, i;
+	unsigned long int next = f1 + f2;
+
+	printf("%lu, %lu, ", f1, f2);
+	for (i = 3; i <= 98; i++)
+	{
+		if (i == 50)
+			printf("%lu\n", next);
+		else
+		{
+			printf("%lu, ", next);
+			f1 = f2;
+			f2 = next;
+			next = f1 + f2;
+		}
+	}
+	return (0);
+}
