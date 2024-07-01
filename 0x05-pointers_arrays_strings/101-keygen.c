c8e50c56c4628d5760ee843cfcd5ce92e1740edd
Antonious-Awad <antonuostony1@gmail.com>
2024-07-01 22:02:34 +0300

    fix: lint

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,17 +9,28 @@
 int main(void)
 {
 	int random;
-	// Value generated from 101-crackme that the code compares the with
-	// the code works as follows:
-	// it takes the ascii value of each integer and sum the ascii value to check if it's equal to 2772, if it's equal then password succeeds
+	/*
+	 * Value generated from 101-crackme that the code compares the with
+	 * the code works as follows:
+	 * it takes the ascii value of each integer
+	 * and sum the ascii value to check if it 's equal to 2772,
+	 * if it' s equal then password succeeds
+	 */
 	int total = 2772;
 
-	// using srand to seed the rand() function with the number of seconds since 1 1 1970
+	/*
+	 *using srand to seed the rand() function
+	 *with the number of seconds since 1 1 1970
+	 */
+
 	srand(time(NULL));
-	// I want to ensure that the loop runs enough times to make the total less than 126
+	/*
+	 * I want to ensure that the loop runs enough times
+	 *to make the total less than 126
+	 */
 	while (total > 125)
 	{
-		// generate random ascii character
+		/* generate random ascii character*/
 		random = (rand() % 126) + 1;
 		printf("%c", random);
 		total -= random;
