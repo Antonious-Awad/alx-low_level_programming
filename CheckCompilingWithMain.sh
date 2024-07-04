18b57986af32bc68aec522989c39d80e8d843827
Antonious-Awad <antonuostony1@gmail.com>
2024-07-04 20:40:30 +0300

    chore: 104

--- CheckCompilingWithMain.sh
+++ CheckCompilingWithMain.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 $1 $2-main.c
+# Syntax example:  alx-c-run 104-print_buffer.c 
+numeric_part=$(echo "$1" | rev | cut -d'-' -f2- | rev)
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 $1 $numeric_part-main.c
 ./a.out
 rm a.out
