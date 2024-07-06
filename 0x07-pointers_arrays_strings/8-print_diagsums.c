5e357ef88b4475dacce9e3fce7f9429b598679cc
Antonious-Awad <antonuostony1@gmail.com>
2024-07-06 11:34:42 +0300

    chore: code fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,22 +8,24 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
-	unsigned int sum1 = 0, sum2 = 0;
+	int i;
+	int sum = 0;
 	int rightMod = size + 1, leftMod = size - 1;
 
-	for (; i < size * size; i++)
+	for (i = 0; i < size * size; i++)
 	{
-		if (i == 0)
-		{
-			sum1 += a[i];
-			continue;
-		}
+
 		if (!(i % rightMod))
-			sum1 += a[i];
-		/*second condition to stop at first index of the last row*/
-		if (!(i % leftMod) && i <= (size * size) - size)
-			sum2 += a[i];
+			sum += a[i];
 	}
-	printf("%u, %u\n", sum1, sum2);
+	printf("%d, ", sum);
+
+	sum = 0;
+	for (i = 0; i < size * size; i++)
+	{
+		/*second condition to stop at first index of the last row*/
+		if (!(i % leftMod) && i != (size * size) - 1 && i != 0)
+			sum += a[i];
+	}
+	printf("%d\n", sum);
 }
