bb1cb71d3906af89105cc9e7b29c80e42e07e473
Antonious-Awad <antonuostony1@gmail.com>
2024-07-12 14:43:28 +0300

    chore: 1

---
+++ 0x0A-argc_argv/1-argc.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument values
+ * Return: always 0
+ */
+
+int main(int argc, char **__attribute__((unused)) argv)
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
