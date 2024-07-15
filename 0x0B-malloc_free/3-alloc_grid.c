92c22b84ad5d45bb02125cf08f1d77fa37fa44f1
Antonious-Awad <antonuostony1@gmail.com>
2024-07-15 22:45:29 +0300

    freeing grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,11 +23,15 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(sizeof(**grid) * width);
 
 		if (grid[i] == NULL)
-			return (NULL);
-		for (j = 0; j < width; j++)
 		{
-			grid[i][j] = 0;
+			for (; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
 		}
+
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
 	return (grid);
 }
