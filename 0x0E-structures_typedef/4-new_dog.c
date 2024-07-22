a267b29c04c2dc4017477bf83b6a3621f596fe5f
Antonious-Awad <antonuostony1@gmail.com>
2024-07-22 22:33:17 +0300

    chore: 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+/**
+ * new_dog - creates a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: dog pointer
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t doggo;
+
+	if (name == NULL || owner == NULL)
+		return NULL;
+
+	doggo.name = name;
+	doggo.age = age;
+	doggo.owner = owner;
+
+	return &doggo;
+}
