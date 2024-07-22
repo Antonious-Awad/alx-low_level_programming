33e3476137f60cb2b926baabfbfcd1176eb7a52b
Antonious-Awad <antonuostony1@gmail.com>
2024-07-22 22:34:13 +0300

    fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,11 +12,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t doggo;
 
 	if (name == NULL || owner == NULL)
-		return NULL;
+		return (NULL);
 
 	doggo.name = name;
 	doggo.age = age;
 	doggo.owner = owner;
 
-	return &doggo;
+	return (&doggo);
 }
