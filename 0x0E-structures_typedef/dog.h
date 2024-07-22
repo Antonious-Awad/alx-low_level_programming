4e5a1cc086a9ce3a905a220d24e77d0c30c087f1
Antonious-Awad <antonuostony1@gmail.com>
2024-07-22 22:16:46 +0300

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: No description
+ */
+
 struct dog
 {
 	char *name;
