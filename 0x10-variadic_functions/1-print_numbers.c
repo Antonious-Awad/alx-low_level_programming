97b7070d3694eb43533010181493ba4be34b4d78
Antonious-Awad <antonuostony1@gmail.com>
2024-07-24 20:51:18 +0300

    chore: change data type

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(numPtr, int));
+		printf("%d", va_arg(numPtr, unsigned int));
 		if (i == n - 1)
 		{
 			printf("\n");
