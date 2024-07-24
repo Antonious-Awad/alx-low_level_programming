1072c4450812935462db567208bacb8dcc5aa8ba
Antonious-Awad <antonuostony1@gmail.com>
2024-07-24 19:55:18 +0300

    chore: 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - addition
+ * @a: first num
+ * @b: second num
+ * Return: Addition
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction
+ * @a: first num
+ * @b: second num
+ * Return: subtraction
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: first num
+ * @b: second num
+ * Return: multiplication
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: first num
+ * @b: second num
+ * Return: Division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - remainder
+ * @a: first num
+ * @b: second num
+ * Return: Remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
