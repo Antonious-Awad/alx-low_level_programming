3056937f8fc2a1b975be67f33bb02cd2241f0938
Antonious-Awad <antonuostony1@gmail.com>
2024-07-24 20:52:37 +0300

    chorer: change loop code

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,15 +18,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numPtr, unsigned int));
-		if (i == n - 1)
-		{
-			printf("\n");
-			continue;
-		}
-		if (separator)
+		if (i < n - 1 && separator)
 		{
 			printf("%s", separator);
+			continue;
 		}
 	}
 	va_end(numPtr);
+	printf("\n");
 }
