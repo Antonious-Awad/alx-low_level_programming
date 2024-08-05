deaaead214cfbfacbda4fda29b1befa255bf0837
Antonious-Awad <antonuostony1@gmail.com>
2024-08-05 21:22:06 +0300

    code fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,7 +7,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-	listint_t *ptr = h;
+	const listint_t *ptr = h;
 	size_t len = 0;
 
 	while (ptr)
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,7 +7,7 @@
 
 size_t listint_len(const listint_t *h)
 {
-	listint_t *ptr = h;
+	const listint_t *ptr = h;
 	size_t len = 0;
 
 	while (ptr)
