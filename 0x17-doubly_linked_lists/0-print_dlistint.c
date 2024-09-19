813b4cdc29b8b35e44ed208085ac884d62c158f0
Antonious-Awad <antonuostony1@gmail.com>
2024-09-19 13:02:25 +0300

    chore:17.0->3

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints a doubly linked list
+ * @h: head node
+ * Return: size of list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	if (!h)
+		return (0);
+
+	while (h->prev)
+		h = h->prev;
+
+	while (h)
+	{
+		printf("%lu", h->n);
+		counter++;
+		h = h->next;
+	}
+
+	return (counter);
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include "stdlib.h"
+/**
+ * add_dnodeint - Adds a node at start of the list
+ * @head: pointer to head node
+ * @n: new value
+ * Return: Updated List
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *current = *head;
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+
+	while (current->prev)
+	{
+		current = current->prev;
+	}
+
+	new->next = current;
+	if (current)
+		current->prev = new;
+
+	*head = new;
+	return (*head);
+}
