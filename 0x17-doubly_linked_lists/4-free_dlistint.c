f1936cb40003346a182e786065a23ee96b33f58b
Antonious-Awad <antonuostony1@gmail.com>
2024-09-19 14:57:06 +0300

    chore:17.4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: head node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+	if (!head)
+		return;
+
+	while (head->prev)
+		head = head->prev;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
